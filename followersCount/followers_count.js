86393bd1b742afb8a427ad28431b689666199837
Rohini2004 <rohinikhan48@gmail.com>
2025-07-07 13:29:35 +0530

    initial commit

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,6 +3,7 @@ function increaseCount() {
   count++; // Increment the count by 1
   displayCount(); // Display the count
   checkCountValue(); // Check count value and display messages
+ 
 }
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
@@ -15,3 +16,9 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+function resetCount() {
+  count = 0;
+  document.getElementById('countDisplay').innerText = count;
+  alert("Followers count has been reset.");
+}
+
